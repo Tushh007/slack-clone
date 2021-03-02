d5af60354175fd95ff254ab3b87f72a7b28d60fb
Tushar Langer <tushar.langer@gmail.com>
2021-03-02 21:41:44 +0530

    setup context api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useState } from 'react';
 import Login from './Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="app">
       <Router>
--- src/Login.js
+++ src/Login.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Login.css';
 import { Button } from '@material-ui/core';
 import { auth, provider } from './firebase';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Login() {
+  const [state, dispatch] = useStateValue();
+
   const signIn = () => {
     auth
       .signInWithPopup(provider)
-      .then((result) => {
-        console.log(result);
+      .then((data) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: data.user,
+        });
       })
       .catch((error) => {
         alert(error.message);
